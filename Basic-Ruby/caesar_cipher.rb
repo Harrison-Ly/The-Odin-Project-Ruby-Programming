2829a545e93ecf2c5937e90feec3d0a864ce9df4
Harrison Ly <harriko@gmail.com>
2016-11-25 14:59:09 +0000

    Add files via upload

---
+++ Basic-Ruby/caesar_cipher.rb
@@ -0,0 +1,21 @@
+def caesar_cipher(string, factor)
+  caesar = string.split("")
+  letter = []
+
+  caesar.each do |c|
+    change = c[0] + factor
+
+    if ("a".."z").include?(c)
+      change > ?z ? change -= 26 : change
+      letter << change.chr
+    elsif ("A".."Z").include?(c)
+      change > ?Z ? change -= 26 : change
+      letter << change.chr
+    else
+      letter << c
+    end
+  end
+  puts letter.join()
+end
+
+caesar_cipher("What a string!", 5)
